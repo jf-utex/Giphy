7197862d6d9b7fb1c6f6213c49520188be52d869
Jeannie <jflaherty@utexas.edu>
2017-08-20 18:16:18 -0500

    still need to pull gifs for added button and would like at least 2 rows of gifs in display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,90 @@
-alert("connected");
-//selects anything with a tag of button
-    $("button").on("click", function() {
-      //assigning button attribute to the button set to data-person
-      var person = $(this).attr("data-person");
-      
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=4f140638c6744217b071e2e6b0d01ab8&limit=10";
 
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-      //got results back
-        .done(function(response) {
-          var results = response.data;
+//Global Variables//
+var userClassic,
+classic,
+queryURL,
+fixedHeightAnimate,
+fixedHeightStill;
 
-          for (var i = 0; i < results.length; i++) {
-            
-            var gifDiv = $("<div class='item'>");
 
-            var rating = results[i].rating;
+// ARRAY FOR BUTTONS
 
-            var p = $("<p>").text("Rating: " + rating);
+var classics = ["MASH 4077", "I Dream of Jeannie", "Gilligan's Island", "The Jetsons", "Mary Tyler Moore", "The Dukes of Hazard", "Growing Pains", "Facts of Life", "Alf", "Teenage Mutant Ninja Turtles"];
 
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
 
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
+// FUNCTIONS
 
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
-        });
+
+// RENDER BUTTONS FUNCTION
+function renderButtons() {
+    $(".buttons").empty();
+
+    for (var i = 0; i < classics.length; i++) {
+        var b = $("<button>");
+        b.addClass("btn btn-primary classics");
+        b.attr("data-name", classics[i]); // Adds a data-attribute
+        b.text(classics[i]);
+        $(".buttons").append(b);
+    }
+}
+
+// ADD NEW USER-GENERATED BUTTON
+function addNewButton() {
+    userButtonVal = $("#new-show").val();
+    classics.push(userButtonVal);  //add to end of array?
+    // renderButtons();
+    var nb = $("<button>");
+    nb.addClass("btn btn-primary new-show");
+    nb.attr("data-name", userButtonVal);
+    nb.text(userButtonVal);
+    $(".buttons").append(nb);
+}
+
+function getGifs() {
+    $.ajax({ url: queryURL, method: 'GET' })
+    .done(function(response) {
+        for (var i = 0; i < response.data.length; i++) 
+        {
+            $(".gifs-display").append("<div class='img-div text-center'><img src='" + response.data[i].images.fixed_height_still.url + "'class='returnedGif' alt='Classic TV Gif' data-still=" + response.data[i].images.fixed_height_still.url + " data-animate=" + response.data[i].images.fixed_height.url + "><p>Rating: " + response.data[i].rating + "</p></div>");
+            $(".returnedGif").attr("data-state", "still");
+        }
+        console.log(response);
     });
+}
+
+
+// CLICK LISTENER FOR ADD NEW USER BUTTON
+
+$(".buttons").on("click", ".classics", function() {
+    classics = $(this).attr("data-name");
+    queryURL = "http://api.giphy.com/v1/gifs/search?q=" + classics + "&limit=10&rating=pg&api_key=4f140638c6744217b071e2e6b0d01ab8";
+    $(".gifs-display").empty();
+    getGifs();
+});
+
+// CLICK LISTENER FOR ADD NEW USER BUTTON
+
+$(".add-classic-btn").on("click", function(e) {
+    e.preventDefault();
+    addNewButton();
+    $("#classics").val("");
+});
+
+// CLICK LISTENER FOR FETCHED GIFS TO PAUSE AND PLAY
+
+$(".gifs-display").on("click", ".returnedGif", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+
+//Run Program
+
+
+renderButtons();
