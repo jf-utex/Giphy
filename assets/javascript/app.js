58af63fad6567014db6ba1015a2c7c2aa39df3ca
Jeannie <jflaherty@utexas.edu>
2017-08-20 08:50:40 -0500

    first push

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+alert("connected");
+//selects anything with a tag of button
+    $("button").on("click", function() {
+      //assigning button attribute to the button set to data-person
+      var person = $(this).attr("data-person");
+      
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+      //got results back
+        .done(function(response) {
+          var results = response.data;
+
+          for (var i = 0; i < results.length; i++) {
+            
+            var gifDiv = $("<div class='item'>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+
+            $("#gifs-appear-here").prepend(gifDiv);
+          }
+        });
+    });
